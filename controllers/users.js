96ab081f5782c9afc95f5dca5d0b95380f35bd87
Hemanth-D <hemanthh531@gmail.com>
2022-11-15 20:48:23 +0530

    13.23: Complete modifying single user info return value based on read query in req url

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,6 @@
-const router = require('express').Router();
+const router = require('express').Router()
 
-const { User, Blog, UserBlogs } = require('../models');
-const { Op } = require('sequelize');
-const { sequelize } = require('../util/db');
+const { User, Blog, UserBlogs } = require('../models')
 
 // GET route for listing all users
 // we return all fields of the user associated with all the blog fields but excluding the userId using attributes, exclude & includes options from sequelize
@@ -11,9 +9,9 @@ router.get('/', async (req, res) => {
     include: {
       model: Blog,
     },
-  });
-  res.json(users);
-});
+  })
+  res.json(users)
+})
 
 // POST route for adding a new user
 router.post('/', async (req, res) => {
@@ -21,12 +19,18 @@ router.post('/', async (req, res) => {
     ...req.body,
     createdAt: new Date(),
     updatedAt: new Date(),
-  });
-  res.json(user);
-});
+  })
+  res.json(user)
+})
 
 // get /api/users/:id route for getting readinglist of users
 router.get('/:id', async (req, res) => {
+  const where = {}
+
+  if (req.query.read) {
+    where.read = req.query.read
+  }
+
   const user = await User.findByPk(req.params.id, {
     attributes: { exclude: ['id', 'createdAt', 'updatedAt'] },
     include: [
@@ -43,26 +47,27 @@ router.get('/:id', async (req, res) => {
           model: UserBlogs,
           as: 'readinglists', // the alias name here should be the same which we created in controllers/index file i.e. 'readinglists'
           attributes: ['read', 'id'],
+          where,
         },
       },
     ],
-  });
-  res.json(user);
-});
+  })
+  res.json(user)
+})
 
 // PUT api/users/:username - changing a username, keep in mind that the parameter is not id but username
 router.put('/:username', async (req, res) => {
   const user = await User.findOne({
     where: { username: req.params.username },
-  });
+  })
   if (user) {
-    user.username = req.body.username;
-    user.updatedAt = new Date();
-    await user.save();
-    res.json(user);
+    user.username = req.body.username
+    user.updatedAt = new Date()
+    await user.save()
+    res.json(user)
   } else {
-    res.status(404).end();
+    res.status(404).end()
   }
-});
+})
 
-module.exports = router;
+module.exports = router
